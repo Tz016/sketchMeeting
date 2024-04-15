9625e7936d883e5cf8420e1f912707ea2c136a30
tz <tz2310@nyu.edu>
2024-04-15 07:17:27 -0400

    upload

--- app.mjs
+++ app.mjs
@@ -1,11 +1,14 @@
+import './config.mjs'
 import express from 'express';
 import http from 'http';
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 import { fileURLToPath } from 'url';
-import { dirname,join } from 'path';
+import { dirname, join } from 'path';
+import { run, createMeet, createUser,addUserToMeeting, findCurrentMeet, findPastMeet, findUser, deletePastMeet, check_closeRoom } from './db.mjs';
 // import {setup, draw} from './src/sketch.js'
 
 
+run()
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -16,16 +19,34 @@ io.on('connection', (socket) => {
     // 发送'init'消息给客户端
     socket.emit('init');
 
+    // //create meeting room
+    // socket.on('create-room', async(data)=>{
+    //     const {roomNumber,username}=data;
+    //     console.log()
+    // })
+
+    //join the room
+    socket.on('join-room',async (data)=>{
+        const {roomNumber,username}=data;
+        const room=await findCurrentMeet(roomNumber);
+        if (room){
+            socket.join(roomNumber);
+            socket.to(roomNumber).emit("user-joined",username);
+            socket.emit('user-list',room.currentUsers);
+        }
+    })
     // 监听客户端发送的消息
-    socket.on('message', (message) => {
-        console.log('Received message:', message);
+    socket.on('sketch', (data) => {
         
+        const {roomNumber,username,fingerTrails}=data;
         // 广播消息给所有客户端
-        io.emit('message', message);
+        io.to(roomNumber).emit('sketch',{username,roomNumber,fingerTrails})
     });
 
     // 监听客户端断开连接事件
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async (data) => {
+        const {roomNumber,username}=data;
+        await check_closeRoom(username,roomNumber);
         console.log('A client disconnected.');
     });
 });
@@ -44,42 +65,118 @@ app.use(express.urlencoded({ extended: true }));
 // })
 app.get('/', (req, res) => {
     res.render('login'); // 返回登录页面
-  });
-
+});
 app.get('/register', (req, res) => {
-    res.render('register'); // 返回注册页面
-  });
+    res.render('register');
+})
 
-  app.post('/login', (req, res) => {
-    const username= req.body.username; // 假设表单中有名为 roomNumber 的字段
-    // 重定向到相应的房间页面
-    res.redirect(`/home/${username}`);
+app.post('/register', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    await createUser({
+        username:username,
+        password:password,
+        meetings: [],
+    })
+    res.redirect(`/`)
+})
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    res.redirect(`/home/${username}`)
+});
+app.get('/home/:username', async (req, res) => {
+    const username = req.params.username;
+    let meetings=[];
+    try {
+        const user = await findUser(username);
+        if(user){
+        if (user.meetings.length > 0) {
+            meetings = await Promise.all(
+                user.meetings.map(async (meetingId) => {
+                    const meeting = await findPastMeet(meetingId);
+                    return meeting;
+                })
+            );
+        } 
+    }
+        res.render('home', { username, meetings });
+    } catch (error) {
+        console.error('获取会议历史时发生错误:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // 房间页面路由，根据房间号动态渲染页面
-app.get('/home/:username', (req, res) => {
-    const username = req.params.username;
-    // 渲染房间页面，传递房间号到页面
-    res.render('home', { username });
-});
+// app.get('/home/:username', async (req, res) => {
+//     const username = req.params.username;
+//     const meetings=await findUser(username);
+//     const history = await Promise.all(
+//         meetings.map(async (meetingId) => {
+//           const meeting = await findPastMeet(meetingId);
+//           return meeting;
+//         })
+//       );
+//     console.log(history)
+//     // 渲染房间页面，传递房间号到页面
+//     res.render('home', { username ,history});
+// });
 
 // 处理表单提交，重定向到房间页面
 app.post('/join-room/:username', (req, res) => {
     const roomNumber = req.body.roomNumber; // 假设表单中有名为 roomNumber 的字段
     const username = req.params.username;
+    addUserToMeeting(username,roomNumber)
     // 重定向到相应的房间页面
     res.redirect(`/room/${roomNumber}/${username}`);
 });
 
+app.post('/create-room/:username', async (req, res) => {
+
+    const roomNumber = req.body.roomNumber; // 假设表单中有名为 roomNumber 的字段
+    const roomName = req.body.roomName;
+    const username = req.params.username;
+    // 重定向到相应的房间页面
+    await createMeet(username, {
+        meetingID: roomNumber,
+        meetingName: roomName,
+        startAt: Date.now(),
+        meetingUsers: [],
+        currentUsers: []
+    })
+    res.redirect(`/room/${roomNumber}/${username}`);
+})
+app.post('/delete/:username/:meetingID', async (req, res) => {
+    const meetingID = req.params.meetingID;
+    const username = req.params.username;
+    try {
+        await deletePastMeet(username, meetingID);
+        res.json({ success: true });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, error: 'Error when deleting the meeting history' });
+    }
+})
+
 // 房间页面路由，根据房间号动态渲染页面
-app.get('/room/:roomNumber/:username', (req, res) => {
+app.get('/room/:roomNumber/:username', async (req, res) => {
     const roomNumber = req.params.roomNumber;
     const username = req.params.username;
+    const room = await findCurrentMeet(roomNumber);
+    const roomName=room.meetingName;
     // 渲染房间页面，传递房间号到页面
-    res.render('room', { roomNumber,username });
+    res.render('room', { roomNumber, username, roomName});
+});
+
+app.post('/close-connection/:roomNumber/:username', async (req, res) => {
+    // 关闭数据库连接的逻辑
+    const username=req.params.username;
+    const roomNumber = req.params.roomNumber
+    await check_closeRoom(username,roomNumber);
+    res.json({ success: true });
+
 });
 
 
 
-
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT ?? 3000);
--- db.mjs
+++ db.mjs
@@ -1,24 +1,180 @@
+import './config.mjs';
 import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import passportLocalMongoose from 'passport-local-mongoose';
+import { Console } from 'console';
 
+mongoose.set('strictQuery', false);
 // 定义 MeetingSchema
+
 const MeetingSchema = new mongoose.Schema({
-    meetingID: {type: String, required: true },
-    meetingName: {type: String, required: true },
-    startAt: { type: Date, required: true },
-    endAt: { type: Date, required: true },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+    meetingID: {type: String, required: true,unique: false },
+    meetingName: {type: String, required: true, unique: false},
+    startAt: { type: Date, required: true, unique: false },
+    endAt: { type: Date, required: false },
+    meetingUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: false}],
+    currentUsers:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' , unique: false}]
 });
 
 // 应用 passportLocalMongoose 插件到 UserSchema
 const UserSchema = new mongoose.Schema({
     // username provided by authentication plugin
     // password hash provided by authentication plugin
-    meetings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' }]
+    username: {type: String, required: true,},
+    password: {type: String, required: true,unique: false},
+    meetings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meeting',unique: false }]
 });
-UserSchema.plugin(passportLocalMongoose);
-MeetingSchema.plugin(passportLocalMongoose);
+
+// UserSchema.plugin(passportLocalMongoose);
+// MeetingSchema.plugin(passportLocalMongoose);
 // 创建 User 和 Meeting 模型
-mongoose.model('User', UserSchema);
-mongoose.model('Meeting', MeetingSchema);
-mongoose.connect(process.env.DSN ?? 'mongodb://localhost/AIT')
+const User=mongoose.model('User', UserSchema);
+const Meeting=mongoose.model('Meeting', MeetingSchema);
+
+
+export async function run() {
+
+     // Connect the client to the server	(optional starting in v4.7)
+      await mongoose.connect(process.env.uri ?? 'mongodb://localhost/AIT');
+      // Send a ping to confirm a successful connection
+      
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+export async function createUser(data){
+    const newUser=new User(data);
+    await newUser.save();
+};
+export async function findCurrentMeet(roomNumber){
+    return await Meeting.findOne({
+        meetingID: roomNumber,
+        endAt: { $exists: false }
+      },'_id meetingName currentUsers').exec();
+};
+
+export async function findPastMeet(_id){
+    const history=await Meeting.findOne({
+        _id: _id,
+        endAt: { $exists: true }
+      }).exec();
+   return history;
+};
+
+export async function findUser(username){
+   const user=await User.findOne({
+    username: username
+  }).exec();
+  if (user) {
+    console.log('User found:', user);
+    // 对找到的用户执行操作
+  } else {
+    console.log('User not found');
+    // 处理用户不存在的情况
+  }
+    return user
+};
+
+
+
+export async function createMeet(username,data){
+    try {
+        const user=await User.findOne({username:username},"_id").exec();
+        data.meetingUsers.push(user._id);
+        data.currentUsers.push(user._id);
+        const newMeet = new Meeting(data);
+        await newMeet.save();
+        await User.updateOne(
+            {
+            username:username
+            },
+            {$push:{meetings:newMeet._id}
+            }
+        )
+        return newMeet;
+      } catch (error) {
+        console.error('Error when creating the meeting:', error);
+        throw error;
+      }
+}
+export async function addUserToMeeting(username,roomNumber){
+  const meeting=await findCurrentMeet(roomNumber);
+  const user=await User.findOne({username:username},"_id").exec();
+  await User.updateOne(
+    {
+    username:username
+    },
+    {$push:{meetings:meeting._id}
+    }
+) 
+await Meeting.updateOne(
+  {
+  _id:meeting._id,
+  },
+  {$push:{currentUsers:user._id},
+  $addToSet:{meetingUsers:user.id}
+  }
+)
+}
+export async function deletePastMeet(username,meetingID){
+    const deletedMeetingID=await Meeting.findOne({_id:meetingID},"_id").exec();
+    await User.updateOne(
+        { username: username },
+        { $pull: { meetings: { $eq: deletedMeetingID._id } } }
+      )
+    console.log(`Meeting is successfully deleted from ${username}\'s history`);
+};
+
+export async function addMeetHistory(meet){
+
+}
+
+export async function check_closeRoom(username,roomNumber) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const meeting = await Meeting.findOne(
+        { meetingID: roomNumber, endAt: { $exists: false } },
+      )
+        .session(session)
+        .exec();
+  
+      let meetingClosed = false;
+      let lastUserLeft = false;
+      if (meeting) {
+        if (meeting.currentUsers.length === 1) {
+          await Meeting.updateOne(
+            { _id: meeting._id },
+            {
+              $set: { endAt: Date.now() },
+              $pull: { currentUsers: meeting.currentUsers[0] }
+            },
+            { session }
+          );
+          meetingClosed = true;
+          lastUserLeft = true;
+        }else{
+        const userID=await findUser(username); 
+        if(userID){
+       
+        await Meeting.updateOne(
+        { _id: meeting._id },
+        {
+          $pull: { currentUsers: userID._id }
+        },
+        { session }
+      );
+      }
+      }
+    }
+    
+  
+      await session.commitTransaction();
+  
+      return { meetingClosed, lastUserLeft };
+    } catch (error) {
+      await session.abortTransaction();
+      console.error('Error checking or closing the room:', error);
+      throw error;
+    } finally {
+      session.endSession();
+    }
+  }
